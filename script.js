310ab51b79e358a876c53063f56d018f08ed736f
Damian Pawela <paweladamian@gmail.com>
2019-03-10 22:22:15 +0100

    Initialized repository

---
+++ script.js
@@ -0,0 +1,25 @@
+const blueBtn = document.querySelector('button.blue');
+const greenBtn = document.querySelector('button.green');
+const redBtn = document.querySelector('button.red');
+const ball = document.querySelector('.ball');
+const shadow = document.querySelector('.shadow');
+
+blueBtn.addEventListener('click', function () {
+    ball.classList.toggle('toggle');
+    shadow.classList.toggle('toggle');
+    document.querySelector('.blueSpan').classList.toggle('change');
+    document.querySelector('.blueSpanHide').classList.toggle('change');
+})
+
+greenBtn.addEventListener('click', function () {
+    ball.classList.toggle('slowMo');
+    shadow.classList.toggle('slowMo');
+    document.querySelector('.greenSpan').classList.toggle('change');
+    document.querySelector('.greenSpanHide').classList.toggle('change');
+})
+
+redBtn.addEventListener('click', function () {
+    ball.classList.toggle('faster');
+    shadow.classList.toggle('faster');
+    this.classList.toggle('change');
+})
